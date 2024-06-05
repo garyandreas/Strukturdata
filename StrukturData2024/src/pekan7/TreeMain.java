443154ddd1eee70bac11273d79c0fa545ff96122
garyandreas <garyandreas0804@gmail.com>
2024-06-05 15:21:50 +0700

    pekan7.0

---
+++ StrukturData2024/src/pekan7/TreeMain.java
@@ -0,0 +1,43 @@
+package pekan7;
+
+public class TreeMain {
+	public static void main (String[] args) {
+		// Membuat Pohon
+		BTree tree = new BTree();
+		System.out.print("Jumlah simpul pohon : ");
+		System.out.println(tree.countNodes());
+		// Menambahkan  simpul data 1
+		Node root = new Node(1);
+		// Menjadikan simpul 1 sebagai root
+		tree.setRoot(root);
+		System.out.println("Jumlah simpul jika hanya ada 1 root");
+		System.out.println(tree.countNodes());
+		Node node2 = new Node(2);
+		Node node3 = new Node(3);
+		Node node4 = new Node(4);
+		Node node5 = new Node(5);
+		Node node6 = new Node(6);
+		Node node7 = new Node(7);
+		root.setLeft(node2);
+		node2.setLeft(node4);
+		node2.setRight(node5);
+		node5.setLeft(node7);
+		root.setRight(node3);
+		node3.setRight(node6);
+		
+		// Set root
+		tree.setCurrent(tree.getRoot());
+		System.out.println("Menampilkan simpul terakhir : ");
+		System.out.println(tree.getCurrent().getData());
+		System.out.println("Jumlah simpul setelah simpul 7 ditambahkan");
+		System.out.println(tree.countNodes());
+		System.out.println("InOrder : ");
+		tree.printInorder();
+		System.out.println("\nPreorder : ");
+		tree.printPreOrder();
+		System.out.println("\nPostorder :");
+		tree.printPostOrder();
+		System.out.println("\nMenampilkan simpul dalam bentuk pohon");
+		tree.print();
+	}
+}
